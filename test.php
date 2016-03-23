b32e0e6f2794efdee618509f2df627182e06be09
Альберт Степанцев <126@gosbroker.ru>
2016-03-23 14:47:36 +0300

    ! wide-screen please!

--- test.php
+++ test.php
@@ -4,8 +4,8 @@ require __DIR__ . '/autoload.php';
 $layout = new \ProfIT\Bbb\Layout(__DIR__ . '/resources/layout.xml', 'defaultlayout');
 
 $image = new \ProfIT\Bbb\layout\Image([
-    'w' => 1024,
-    'h' => 768
+    'w' => 1280,
+    'h' => 720
 ]);
 $image->loadLayout($layout);
 $image->generatePng('test.png');
