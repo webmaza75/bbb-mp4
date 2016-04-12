95c2806530eab675d065b3c812169bae3660f201
Irina Rabinovich <webmaza75@gmail.com>
2016-04-12 14:08:50 +0300

    + команда test.php c 2 обязательными и 2 необязательными параметрами

--- test.php
+++ test.php
@@ -1,12 +1,26 @@
 <?php
+/**
+ * @use php test.php --src=./resources/layout.xml --css=./resources/style/css/BBBDefault.css --width=1280 --height=720
+ * php test.php --src=./resources/layout.xml --css=./resources/style/css/BBBDefault.css
+ */
+//
+$options = getopt('', ['src:', 'css:', 'width::', 'height::']);
+// Источник src и css
+$resFileName = realpath($options['src']);
+$cssFileName = realpath($options['css']);
+$w = $options['width'] ? : 1280;
+$h = $options['height'] ? : 720;
+
 
 require __DIR__ . '/autoload.php';
-$layout = new \ProfIT\Bbb\Layout(__DIR__ . '/resources/layout.xml', 'defaultlayout');
-$layout->setStyleSheet(__DIR__ . '/resources/style/css/BBBDefault.css');
 
+$s = fopen($resFileName, 'r');
+$layout = new \ProfIT\Bbb\Layout($resFileName, 'defaultlayout');
+$layout->setStyleSheet($cssFileName);
 $image = new \ProfIT\Bbb\layout\Image([
-    'w' => 1280,
-    'h' => 720
+    'w' => $w,
+    'h' => $h
 ]);
+
 $image->loadLayout($layout);
 $image->generatePng('test.png');
